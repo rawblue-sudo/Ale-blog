c70412b5e48525905c3377f06cdd5a949e27027f
rawblue-sudo <56031736+rawblue-sudo@users.noreply.github.com>
2020-05-04 19:10:00 +0200

    fixed query

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -2,7 +2,7 @@
   <Layout>
     <h2>Latest blog posts</h2>
     <ul>
-      <li v-for="posts in $page.post" :key="edge.node.id">{{ post.title }}</li>
+      <li v-for="posts in $page.post" :key="post.id">{{ post.title }}</li>
     </ul>
   </Layout>
 </template>
