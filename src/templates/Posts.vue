25bd345664763bf30c75794968fbb3b4cfe29c7f
rawblue-sudo <56031736+rawblue-sudo@users.noreply.github.com>
2020-05-04 18:35:05 +0200

    fixed post

--- src/templates/Posts.vue
+++ src/templates/Posts.vue
@@ -1,21 +1,19 @@
 <!-- src/templates/Post.vue -->
 <template>
   <Layout>
-    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
-      <h2>{{ edge.node.title }}</h2>
+    <div>
+      <h1 v-html="$page.post.title" />
+      <div v-html="$page.post.content" />
     </div>
   </Layout>
 </template>
 
 <page-query>
 query ($id: ID!) {
-  edges {
-    node {
       post(id: $id) {
         title
         content
-      }
-    }
+  
   }
 }
 </page-query>
